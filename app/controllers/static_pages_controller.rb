5114ec7db641440c664569f563ab89f5bf1c8c99
shosho2 <shashkovq@gmail.com>
2017-05-02 22:56:14 +0400

    update Gemfile.lock for Heroku

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def services
+  end
+
+  def price
+  end
+
+  def help
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  get 'static_pages/services'
+
+  get 'static_pages/price'
+
+  get 'static_pages/about'
+
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get "home/index"
+
+  root to: 'static_pages#home'
+end
