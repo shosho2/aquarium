10b924006fe49df648e5c92a4f21f6b05ca67026
shosho2 <shashkovq@gmail.com>
2017-05-11 14:16:23 +0400

    add jquery code for Ti

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
+  get 'static_pages/ti'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get "home/index"
 
